c690bb64153aa04d4b45180a8978fba253372781
BenMillard <ben@sitesurgeon.co.uk>
2019-07-04 22:58:24 +0100

    Reverted April 2019 green for comparison with Andy's photography; first two carousel slides both have 2 buttons; second button URL is from slide excerpt.

--- element-templates/carousel.php
+++ element-templates/carousel.php
@@ -2,11 +2,16 @@
   <ol class="carousel-indicators">
     <li data-target="#carouselControls" data-slide-to="0" class="active"></li>
     <li data-target="#carouselControls" data-slide-to="1"></li>
-    <li data-target="#carouselControls" data-slide-to="2"></li>
-
+    <!-- <li data-target="#carouselControls" data-slide-to="2"></li> -->
+    <!-- <li data-target="#carouselControls" data-slide-to="3"></li> -->
   </ol>
 
+  <!-- Carousel -->
   <div class="carousel-inner">
+
+
+
+    <!-- Carousel: Slide 1 -->
     <div class="carousel-item active">
       <?php
         $post_object = get_field('slide_1');
@@ -17,9 +22,9 @@
           $img = wp_get_attachment_url( get_post_thumbnail_id($post), 'thumbnail' );
         ?>
       <?php if($img) { ?>
-      <img class="d-block w-100" src="<?php echo $img ?>" alt="first slide">
+      <img class="d-block w-100" src="<?php echo $img ?>" alt="">
       <?php } else { ?>
-      <img class="d-block w-100" src="http://via.placeholder.com/1040x500" alt="first slide">
+      <img class="d-block w-100" src="http://via.placeholder.com/1040x500" alt="">
       <?php } ?>
       <div class="carousel-caption card d-none d-md-block">
           <div class="card-body">
@@ -34,7 +39,7 @@
             </p>
             <p>
                 <a href="<?php echo the_field('slide_page_link'); ?>" class="btn btn-lg">Free ride for Adult</a>
-                <a href="<?php echo $post->post_excerpt; ?>" class="btn btn-lg btn-success">Free ride for Youth</a>
+                <a href="<?php echo $post->post_excerpt; ?>" class="btn btn-lg btn-success">Join as an Adult</a>
             </p>
           </div>
       </div>
@@ -42,6 +47,9 @@
       <?php endif; ?>
     </div>
 
+
+
+    <!-- Carousel: Slide 2 -->
     <div class="carousel-item">
       <?php
                     $post_object = get_field('slide_2');
@@ -52,9 +60,9 @@
                       $img = wp_get_attachment_url( get_post_thumbnail_id($post), 'thumbnail' );
                     ?>
       <?php if($img) { ?>
-      <img class="d-block w-100" src="<?php echo $img ?>" alt="first slide">
+      <img class="d-block w-100" src="<?php echo $img ?>" alt="">
       <?php } else { ?>
-      <img class="d-block w-100" src="http://via.placeholder.com/1040x500" alt="first slide">
+      <img class="d-block w-100" src="http://via.placeholder.com/1040x500" alt="">
       <?php } ?>
       <div class="carousel-caption card d-none d-md-block">
           <div class="card-body">
@@ -67,16 +75,20 @@
             <p class="card-text">
               <?php echo $post->post_content; ?>
             </p>
-            <p><a class="btn btn-lg" href="<?php echo the_field('slide_page_link'); ?>">Join our Club</a></p>
+            <p>
+                <a href="<?php echo the_field('slide_page_link'); ?>" class="btn btn-lg">Free ride for Youth</a>
+                <a href="<?php echo $post->post_excerpt; ?>" class="btn btn-lg btn-success">Join as a Youth</a>
+            </p>
           </div>
       </div>
       <?php wp_reset_postdata(); // IMPORTANT - reset the $post™™£ object so the rest of the page works correctly ?>
       <?php endif; ?>
     </div>
-
-
-
-
+    
+    
+    
+    <!-- Carousel: Slide 3 -->
+    <!--
     <div class="carousel-item">
       <?php
         $post_object = get_field('slide_3');
@@ -87,9 +99,9 @@
           $img = wp_get_attachment_url( get_post_thumbnail_id($post), 'thumbnail' );
         ?>
       <?php if($img) { ?>
-      <img class="d-block w-100" src="<?php echo $img ?>" alt="first slide">
+      <img class="d-block w-100" src="<?php echo $img ?>" alt="">
       <?php } else { ?>
-      <img class="d-block w-100" src="http://via.placeholder.com/1040x500" alt="first slide">
+      <img class="d-block w-100" src="http://via.placeholder.com/1040x500" alt="">
       <?php } ?>
       <div class="carousel-caption card d-none d-md-block">
           <div class="card-body">
@@ -102,8 +114,38 @@
       <?php wp_reset_postdata(); // IMPORTANT - reset the $post™™£ object so the rest of the page works correctly ?>
       <?php endif; ?>
     </div>
-
-
+    -->
+    
+    
+    
+    <!-- Carousel: Slide 3 -->
+    <!--
+    <div class="carousel-item">
+      <?php
+        $post_object = get_field('slide_4');
+        if( $post_object ):
+         // override $post
+          $post = $post_object;
+          setup_postdata( $post );
+          $img = wp_get_attachment_url( get_post_thumbnail_id($post), 'thumbnail' );
+        ?>
+      <?php if($img) { ?>
+      <img class="d-block w-100" src="<?php echo $img ?>" alt="">
+      <?php } else { ?>
+      <img class="d-block w-100" src="http://via.placeholder.com/1040x500" alt="">
+      <?php } ?>
+      <div class="carousel-caption card d-none d-md-block">
+          <div class="card-body">
+            <p class="card-tag"><?php the_field('slide_tag'); ?></p>
+            <h2 class="slide-text"><?php the_title(); ?></h2>
+            <p class="card-text"><?php echo $post->post_content; ?></p>
+            <p><a class="btn btn-lg" href="<?php echo the_field('slide_page_link'); ?>">Join our Club</a></p>
+          </div>
+      </div>
+      <?php wp_reset_postdata(); // IMPORTANT - reset the $post™™£ object so the rest of the page works correctly ?>
+      <?php endif; ?>
+    </div>
+    -->
 
 
 
