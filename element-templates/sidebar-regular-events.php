0a7b84347561f6e55316aaab9f44e6ed318e788e
BenMillard <ben@sitesurgeon.co.uk>
2019-10-12 01:07:11 +0100

    Regular Events sidebar for Events section content updates.

--- element-templates/sidebar-regular-events.php
+++ element-templates/sidebar-regular-events.php
@@ -2,18 +2,18 @@
     <h3>Weekly Events</h3>
     
     <h4>Tuesdays &amp; Thursdays</h4>
-    <p><a href="<?php echo site_url('/about-us/sponsors'); ?>">7:30pm TurboBeat with EliteCycling</a></p>
-    <p>An excellent way to get fitter. For more information and to book a place, please click below.</p>
+    <p>7:30pm TurboBeat with <a href="<?php echo site_url('/about-us/sponsors/elitecycling/'); ?>">EliteCycling</a></p>
+    <p>An excellent way to get fitter. Energetic classes where you attach your road bicycle to a turbo trainer indoors at Crystal Palace Park.</p>
     
     <h4>Wednesdays</h4>
-    <p><a href="<?php echo site_url('/about-us/sponsors/se20-cycles/'); ?>">7pm SE20 Cycles Shop Rides</a></p>
-    <p>Balanced pace around 13mph for competent cyclists with fairly short stops.</p>
+    <p>7pm <a href="<?php echo site_url('/about-us/sponsors/se20-cycles/'); ?>">SE20 Cycles</a> Shop Rides</p>
+    <p>Medium paced rides that run from  when the evenings are light. Details for these rides are published on <a href="https://www.facebook.com/groups/206843416040902/">our Facebook page</a> when they are running.</p>
     
     <h4>Thursdays</h4>
-    <p><a href="<?php echo site_url('/about-us/sponsors/se20-cycles/'); ?>">7:30pm SE20 Cycles Shop Rides</a></p>
-    <p>Can't wait till the next Sunday club ride? Join a mid week ride.</p>
+    <p>7:30pm <a href="<?php echo site_url('/about-us/sponsors/se20-cycles/'); ?>">SE20 Cycles</a> Shop Rides</p>
+    <p>Turn up on Thursday night for our fastest rides, all year round. Join the <a href="https://www.facebook.com/groups/684221545036691/">SE20 Cycles shop ride Facebook group</a> to check when this ride is on.</p>
     
     <h4>Sundays</h4>
-    <p><a href="<?php echo site_url('/adults/sunday-morning-rides/'); ?>">7:45am Sunday Morning Club Rides</a></p>
+    <p>7:45am <a href="<?php echo site_url('/adults/sunday-morning-rides/'); ?>">Sunday Morning Club Rides</a></p>
     <p>Book early for one of our popular rides with a choice of distance, speed and start time.</p>
 </div>
