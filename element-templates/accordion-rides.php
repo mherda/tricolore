2b31af6374bea2f6dbec5254fbf86310706bd803
Marcin Herda <mherda@gmail.com>
2019-12-09 19:55:12 +0000

    # -> url on mobile

--- element-templates/accordion-rides.php
+++ element-templates/accordion-rides.php
@@ -14,7 +14,7 @@
             <tr id="heading1" class="b-green" data-toggle="collapse" data-target="#collapse1" aria-expanded="true">
                 <th><i class="fa"></i> Steady</th>
                 <td>8:15am</td>
-                <td>10mph</td>
+                <td>10-11mph</td>
                 <td>24&ndash;27 miles</td>
                 <td>4 hours</td>
             </tr>
@@ -27,7 +27,7 @@
             <tr id="heading2" class="b-blue collapsed" data-toggle="collapse" data-target="#collapse2">
                 <th><i class="fa"></i> Short 1</th>
                 <td>8:10am</td>
-                <td>11mph</td>
+                <td>11-12mph</td>
                 <td>30&ndash;35 miles</td>
                 <td>4½ hours</td>
             </tr>
@@ -40,7 +40,7 @@
             <tr id="heading3" class="b-blue collapsed" data-toggle="collapse" data-target="#collapse3">
                 <th><i class="fa"></i> Short 2</th>
                 <td>8am</td>
-                <td>12mph</td>
+                <td>12-13mph</td>
                 <td>30&ndash;35 miles</td>
                 <td>4 hours</td>
             </tr>
@@ -53,7 +53,7 @@
             <tr id="heading4" class="b-red collapsed" data-toggle="collapse" data-target="#collapse4">
                 <th><i class="fa"></i> Short 3</th>
                 <td>7:50am</td>
-                <td>13mph</td>
+                <td>13-14mph</td>
                 <td>30&ndash;35 miles</td>
                 <td>3 hours</td>
             </tr>
@@ -66,7 +66,7 @@
             <tr id="heading5" class="b-black collapsed" data-toggle="collapse" data-target="#collapse5">
                 <th><i class="fa"></i> Short 4</th>
                 <td>7:45am</td>
-                <td>16mph+</td>
+                <td>15-16mph</td>
                 <td>30&ndash;35 miles</td>
                 <td>2½ hours</td>
             </tr>
@@ -76,58 +76,84 @@
                     <b>Can get quite competitive.</b> New members should experience an easier Short group group first.
                 </td>
             </tr>
+            <tr id="heading6" class="b-black collapsed" data-toggle="collapse" data-target="#collapse6">
+                <th><i class="fa"></i> Short 5 (ad hoc)</th>
+                <td>7:45am</td>
+                <td>17mph+</td>
+                <td>30&ndash;35 miles</td>
+                <td>2½ hours</td>
+            </tr>
+            <tr id="collapse6" class="b-black collapse" data-parent="#accordion">
+                <td colspan="6">
+                    Self-led, ad hoc
+                </td>
+            </tr>
             
         </tbody>
         <tbody>
             
-            <tr id="heading6" class="b-blue collapsed" data-toggle="collapse" data-target="#collapse6">
-                <th><i class="fa"></i> Long 1</th>
+            <tr id="heading7" class="b-blue collapsed" data-toggle="collapse" data-target="#collapse7">
+                <th><i class="fa"></i> Long 1 (ad hoc)</th>
                 <td>8:05am</td>
-                <td>11mph</td>
+                <td>11-12mph</td>
                 <td>45&ndash;55 miles</td>
                 <td>5½ hours</td>
             </tr>
-            <tr id="collapse6" class="b-blue collapse" data-parent="#accordion">
+            <tr id="collapse7" class="b-blue collapse" data-parent="#accordion">
                 <td colspan="6">
                     Longer distance at a more relaxed pace. This is a ride for those who normally do the Steady, or Short 1 or 2 but who fancy a longer ride. It’s great if you have signed up for a big sportive and need some extra mileage.<br />
                     <b>Note:</b> This ride may be cancelled when interest drops over Winter.
                 </td>
             </tr>
 
-            <tr id="heading7" class="b-blue collapsed" data-toggle="collapse" data-target="#collapse7">
+            <tr id="heading8" class="b-blue collapsed" data-toggle="collapse" data-target="#collapse8">
                 <th><i class="fa"></i> Long 2</th>
                 <td>8am</td>
-                <td>13mph</td>
+                <td>13-14mph</td>
                 <td>45&ndash;55 miles</td>
                 <td>5 hours</td>
             </tr>
-            <tr id="collapse7" class="b-blue collapse" data-parent="#accordion">
+            <tr id="collapse8" class="b-blue collapse" data-parent="#accordion">
                 <td colspan="6">
                     The pace is getting quicker and the ride more experienced ride leaders will throw in some extra challenges. Group riding is encouraged and the rides will include some technical sections and steep hills.
                 </td>
             </tr>
 
-            <tr id="heading8" class="b-red collapsed" data-toggle="collapse" data-target="#collapse8">
+            <tr id="heading9" class="b-red collapsed" data-toggle="collapse" data-target="#collapse9">
                 <th><i class="fa"></i> Long 3</th>
                 <td>7:50am</td>
-                <td>15mph</td>
+                <td>15-16mph</td>
                 <td>45&ndash;55 miles</td>
                 <td>4½ hours</td>
             </tr>
-            <tr id="collapse8" class="b-red collapse" data-parent="#accordion">
+            <tr id="collapse9" class="b-red collapse" data-parent="#accordion">
                 <td colspan="6">
                     Suitable for more experienced and quicker riders who are familiar with group riding. This will definitely include some challenging climbs and some fast sections.
                 </td>
             </tr>
 
-            <tr id="heading9" class="b-black collapsed" data-toggle="collapse" data-target="#collapse9">
+            <tr id="heading10" class="b-black collapsed" data-toggle="collapse" data-target="#collapse10">
                 <th><i class="fa"></i> Long 4</th>
                 <td>7:45am</td>
                 <td>17mph+</td>
                 <td>60&ndash;75 miles</td>
                 <td>4½ to 5 hours</td>
             </tr>
-            <tr id="collapse9" class="b-black collapse" data-parent="#accordion">
+            <tr id="collapse10" class="b-black collapse" data-parent="#accordion">
+                <td colspan="6">
+                    Suitable for members who race or are training for challenging sportives.<br />
+                    <b>Can get quite competitive.</b> New members should experience an easier Long group first.
+                </td>
+            </tr>
+
+            <tr id="heading11" class="b-black collapsed" data-toggle="collapse" data-target="#collapse11">
+                <th><i class="fa"></i> Long 5 (ad hoc)</th>
+                <td>7:45am</td>
+                <td>18mph+</td>
+                <td>60&ndash;75 miles</td>
+                <td>4½ to 5 hours</td>
+            </tr>
+            <tr id="collapse11" class="b-black collapse" data-parent="#accordion">
                 <td colspan="6">
                     Suitable for members who race or are training for challenging sportives.<br />
                     <b>Can get quite competitive.</b> New members should experience an easier Long group first.
@@ -137,10 +163,10 @@
         </tbody>
         <tbody>
             
-            <tr id="heading10" class="collapsed" data-toggle="collapse" data-target="#collapse10">
+            <tr id="heading12" class="collapsed" data-toggle="collapse" data-target="#collapse12">
                 <th colspan="5"><i class="fa"></i> Special</th>
             </tr>
-            <tr id="collapse10" class="collapse" data-parent="#accordion">
+            <tr id="collapse12" class="collapse" data-parent="#accordion">
                 <td colspan="6">
                   We don’t run these every week, so look out for announcements. They are aimed at experienced and fit riders looking to train over a longer distance, perhaps for an organised sportive event.<br />
                   They will often leave earlier than the other rides and be out for up to 6 hours, moving at an average 13-15mph. They will definitely take in some of the local classics, like Leith Hill, White Down, Toys Hill and go out to more far flung places like the Ashdown Forest.
