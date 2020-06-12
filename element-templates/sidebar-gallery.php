8f5a98ca68e415da034f733fa9cc880a2fce8678
BenMillard <ben@sitesurgeon.co.uk>
2020-06-12 19:30:25 +0100

    Sidebar gallery is not shown if there is no Flickr tag; Flickr link goes directly to album if there is a tag; spacing reduced between Flickr logo and text.

--- element-templates/sidebar-gallery.php
+++ element-templates/sidebar-gallery.php
@@ -15,17 +15,11 @@ if ( $tag ) {
     if ( $glink ) {
     ?>
     <div class="gallery">
-        <h3>Event Gallery</h3>
+        <h3>Event Gallery on <a href="<?php the_field('gallery_link'); ?>"><i class="fab fa-lg fa-flickr"></i>Flickr</a></h3>
         <?php $queried_object = get_queried_object(); ?>
         <p><?php echo $queried_object->post_title; ?></p>
-        <p>More photos from the event are on Flickr.</p>
-        <p>
-            <a href="<?php the_field('gallery_link'); ?>" class="btn">Event Gallery</a>
-        </p>
     </div>
     <?php } else { ?>
         <!-- No Flickr tag supplied. -->
-        <?php echo do_shortcode("[flickr_tags user_id='69040456@N07' tags='PengeCycleClub' max_num_photos='6']"); ?>
-
     <?php } // end elseif ?>
 <?php } // end if ?>
