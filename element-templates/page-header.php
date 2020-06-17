abc685b7d415d4e0d23675c86a5ca56e8dbe794d
Marcin Herda <mherda@gmail.com>
2020-06-17 23:35:55 +0100

    The 'page' template is used as a failsafe template in case someone creates a page and does not explicitly set a template. Updated it with 'Generic Page' content. Removed the redundant page-header-generic

--- element-templates/page-header.php
+++ element-templates/page-header.php
@@ -30,7 +30,3 @@
 	}
 ?>
 
-<!-- Page title -->
-<header class="entry-header">
-	<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-</header><!-- .entry-header -->
--- page.php
+++ page.php
@@ -10,47 +10,69 @@
  * @package understrap
  */
 
-get_header();
 
+
+ get_header('front');
 $container = get_theme_mod( 'understrap_container_type' ); // echo esc_attr( $container );
 
 ?>
-
 <div class="wrapper" id="page-wrapper">
+  <div class="<?php echo esc_attr( $container ); ?> notFull" id="content">
+      <div class="content-area" id="primary">
+        <main class="site-main" id="main" role="main">
 
-	<div class="<?php echo esc_attr( $container ); ?> notFull" id="content">
+          <!-- Top Row  -->
 
-		<div class="row">
+          <div class="row">
+            <div class="col-md-8">
+              <!-- beginning of the main column -->
+              
+              <!-- Breadcrumb, hero image then title -->
+              <?php get_template_part( 'element-templates/page-header' ); ?>
+              
+              
+              
+              <div class="entry-content">
+                  
+          		  <?php
+        		  global $post;
+        		  $content = apply_filters('the_content', $post->post_content);
+        		  echo $content;
+        		  ?>
+                  
+              </div><!-- .entry-content -->
+              
+              <?php get_template_part( 'element-templates/bottom_tiles1' ); ?>
 
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
 
-			<main class="site-main" id="main">
+            </div> <!-- end of main column -->
 
-				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php get_template_part( 'loop-templates/content', 'page' ); ?>
+            <div class="col-md-4 sidebar"> <!-- beginning of Sidebar -->
+                
+				<?php get_template_part( 'element-templates/sidebar-gallery' ); ?>
+                
+				<?php get_template_part( 'element-templates/sidebar-upcoming-events' ); ?>
+                
+                <?php get_template_part( 'sidebar-templates/sidebar', 'right' ); ?>
+                
+            </div> <!-- end of right column -->
 
-					<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-					?>
 
-				<?php endwhile; // end of the loop. ?>
+          </div> <!-- end of Top Row-->
 
-			</main><!-- #main -->
 
-		</div><!-- #primary -->
 
-		<!-- Do the right sidebar check -->
-		<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+        </main><!-- #main -->
 
-	</div><!-- .row -->
+      </div><!-- #primary -->
 
-</div><!-- Container end -->
+
+
+
+
+  </div><!-- Container end -->
 
 </div><!-- Wrapper end -->
 
-<?php get_footer(); ?>
+<?php get_footer('main'); ?>
