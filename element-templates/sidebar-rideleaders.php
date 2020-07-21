fe9cbda569d3a2fb09116f73aa338504ce6803b0
Marcin Herda <mherda@gmail.com>
2020-07-21 20:11:21 +0100

    Updated rideleader notes links

--- element-templates/sidebar-rideleaders.php
+++ element-templates/sidebar-rideleaders.php
@@ -1,6 +1,6 @@
 <div>
     <h3>Ride Leaders</h3>
     <p><a class="btn" href="https://docs.google.com/spreadsheet/pub?key=0Akw03_pqABHedHlWbU5Sb1M4N0hNcDVWVzBSMXB1SWc&output=html">Leader rotas</a></p>
-    <p><a class="btn" href="https://docs.google.com/document/d/13L991t63X_M4vBtN-t026OWX-FB8xYRt2LxdUd6Cmao/edit">Leader notes</a></p>
-    <p><a class="btn" href="http://pengecycleclub.org.uk/wp-content/uploads/2018/04/incident-report-form-716-copy.pdf">Incident form</a></p>
+    <p><a class="btn" href="/ride-leader-notes/">Leader notes</a></p>
+    <p><a class="btn" href="<?php echo get_template_directory_uri(); ?>/assets/forms/incident-report-form-716.pdf">Incident form</a></p>
 </div>
